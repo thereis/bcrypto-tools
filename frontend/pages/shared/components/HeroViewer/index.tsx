e76eb5ef2890196fd2451789ab8f102119dc8487
Lucas Reis <thereis@live.com>
2021-12-26 13:19:04 +0100

    feat: table improvements

--- frontend/pages/shared/components/HeroViewer/index.tsx
+++ frontend/pages/shared/components/HeroViewer/index.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+
+/**
+ * Dependencies
+ */
 import { Hero, Rarity } from "../../../../core/models/hero";
+import { Column, useSortBy, useTable } from "react-table";
+
+/**
+ * Components
+ */
 import Alert from "../Alert";
 
 const translateRarity = (rarity: Rarity) => {
@@ -26,41 +35,89 @@ type Props = {
 };
 
 const HeroViewer: React.FC<Props> = ({ heroes }) => {
-  const headers = [
-    "ID",
-    "Index",
-    "Rarity",
-    "Level",
-    "Color",
-    "Skin",
-    "Stamina",
-    "Speed",
-    "Bomb Skin",
-    "Bomb Count",
-    "Bomb Power",
-    "Bomb Range",
-    "Ability",
-    "Abilities",
-  ];
+  const columns: Column<Hero>[] = React.useMemo(
+    () => [
+      {
+        Header: "Data",
+        columns: [
+          {
+            Header: "ID",
+            accessor: "id",
+          },
+          {
+            Header: "Index",
+            accessor: "index",
+          },
+        ],
+      },
+      {
+        Header: "Hero Stats",
+        columns: [
+          {
+            Header: "Rarity",
+            accessor: "rarity",
+            Cell: ({ cell: { value } }: any) => translateRarity(value),
+          },
+          {
+            Header: "Level",
+            accessor: "level",
+          },
+          {
+            Header: "Color",
+            accessor: "color",
+          },
+          {
+            Header: "Skin",
+            accessor: "skin",
+          },
+          {
+            Header: "Stamina",
+            accessor: "stamina",
+          },
+          {
+            Header: "Speed",
+            accessor: "speed",
+          },
+          {
+            Header: "Bomb Skin",
+            accessor: "bombSkin",
+          },
+          {
+            Header: "Bomb Count",
+            accessor: "bombCount",
+          },
+          {
+            Header: "Bomb Power",
+            accessor: "bombPower",
+          },
+          {
+            Header: "Bomb Range",
+            accessor: "bombRange",
+          },
+          {
+            Header: "Ability",
+            accessor: "ability",
+          },
+          {
+            Header: "Abilities",
+            accessor: "abilities",
+          },
+        ],
+      },
+    ],
+    []
+  );
 
-  const rows = heroes.map((hero) => {
-    return [
-      hero.id,
-      hero.index,
-      translateRarity(hero.rarity),
-      hero.level,
-      hero.color,
-      hero.skin,
-      hero.stamina,
-      hero.speed,
-      hero.bombSkin,
-      hero.bombCount,
-      hero.bombPower,
-      hero.bombRange,
-      hero.ability,
-      hero.abilities,
-    ];
-  });
+  const generateData = (heroes: Hero[]) => {
+    return heroes.map((hero: Hero) => {
+      return hero;
+    });
+  };
+
+  const data = React.useMemo(() => generateData(heroes), []);
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data }, useSortBy);
 
   if (!heroes.length)
     return (
@@ -76,37 +133,56 @@ const HeroViewer: React.FC<Props> = ({ heroes }) => {
       <div className="flex flex-col mt-6">
         <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block py-2 min-w-full sm:px-6 lg:px-8">
-            <div className="overflow-hidden shadow-md sm:rounded-lg">
-              <table className="min-w-full">
+            <div className="overflow-hidden shadow-md">
+              <table className="min-w-full" {...getTableProps()}>
                 <thead className="bg-gray-50 dark:bg-gray-700">
-                  <tr>
-                    {headers.map((header) => (
-                      <th
-                        key={header}
-                        scope="col"
-                        className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
-                      >
-                        {header}
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {rows.map((row, index) => (
-                    <tr
-                      key={index}
-                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-                    >
-                      {row.map((item, index) => (
-                        <td
-                          key={`item_${index}`}
-                          className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                      {headerGroup.headers.map((column: any) => (
+                        <th
+                          {...column.getHeaderProps(
+                            column.getSortByToggleProps()
+                          )}
+                          className=" py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
                         >
-                          {item}
-                        </td>
+                          {column.render("Header")}
+
+                          <span>
+                            {column.isSorted
+                              ? column.isSortedDesc
+                                ? " 🔽"
+                                : " 🔼"
+                              : ""}
+                          </span>
+                        </th>
                       ))}
                     </tr>
                   ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                  {rows.map((row) => {
+                    prepareRow(row);
+
+                    const className =
+                      row.index % 2 === 0
+                        ? "bg-white border-b dark:bg-gray-800 dark:border-gray-600"
+                        : "bg-gray-50 border-b dark:bg-gray-700 dark:border-gray-600";
+
+                    return (
+                      <tr className={className} {...row.getRowProps()}>
+                        {row.cells.map((cell) => {
+                          return (
+                            <td
+                              {...cell.getCellProps()}
+                              className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                            >
+                              {cell.render("Cell")}
+                            </td>
+                          );
+                        })}
+                      </tr>
+                    );
+                  })}
                 </tbody>
               </table>
             </div>
